d4904e4dc2b349cf7a96595049b6f229028c52b0
Armin Preiml <apreiml@strohwolke.at>
2021-01-12 10:13:06 +0100

    remove barcode writer dependency

--- src/Vendor/GoogleAuth.php
+++ src/Vendor/GoogleAuth.php
@@ -2,9 +2,7 @@
 declare(strict_types=1);
 namespace ParagonIE\MultiFactor\Vendor;
 
-use \BaconQrCode\Writer;
 use \ParagonIE\ConstantTime\Base32;
-use \ParagonIE\MultiFactor\FIDOU2F;
 use ParagonIE\MultiFactor\OneTime;
 use \ParagonIE\MultiFactor\OTP\{
     HOTP,
@@ -17,54 +15,6 @@ use \ParagonIE\MultiFactor\OTP\{
  */
 class GoogleAuth extends OneTime
 {
-
-    /**
-     * @var int
-     */
-    public $defaultQRCodeHeight = 384;
-
-    /**
-     * @var int
-     */
-    public $defaultQRCodeWidth = 384;
-
-    /**
-     * Create a QR code to load the key onto the device
-     *
-     * @param Writer $qrCodeWriter
-     * @param string $outFile        Where to store the QR code?
-     * @param string $username       Username or email address
-     * @param string $issuer         Optional
-     * @param string $label          Optional
-     * @param int $initialCounter    Initial counter value
-     * @return void
-     * @throws \Exception
-     */
-    public function makeQRCode(
-        Writer $qrCodeWriter = null,
-        string $outFile = 'php://output',
-        string $username = '',
-        string $issuer = '',
-        string $label = '',
-        int $initialCounter = 0
-    ) {
-        $message = $this->makeQRCodeMessage($username, $issuer, $label, $initialCounter);
-
-        $this->makeQRCodeWriteOrDefault($qrCodeWriter)->writeFile($message, $outFile);
-    }
-
-    public function getQRCode(
-        Writer $qrCodeWriter = null,
-        string $username = '',
-        string $issuer = '',
-        string $label = '',
-        int $initialCounter = 0
-    ) : string {
-        $message = $this->makeQRCodeMessage($username, $issuer, $label, $initialCounter);
-
-        return $this->makeQRCodeWriteOrDefault($qrCodeWriter)->writeString($message);
-    }
-
     public function makeQRCodeMessage(
         string $username = '',
         string $issuer = '',
@@ -103,18 +53,5 @@ class GoogleAuth extends OneTime
         $message .= '?' . \http_build_query($args);
 
         return $message;
-    }
-
-    protected function makeQRCodeWriteOrDefault(Writer $qrCodeWriter = null) : Writer
-    {
-        // Sane default; You can dependency-inject a replacement:
-        if (!$qrCodeWriter) {
-            $renderer = new \BaconQrCode\Renderer\Image\Png();
-            $renderer->setHeight($this->defaultQRCodeWidth);
-            $renderer->setWidth($this->defaultQRCodeHeight);
-            $qrCodeWriter = new \BaconQrCode\Writer($renderer);
-        }
-
-        return $qrCodeWriter;
     }
 }
